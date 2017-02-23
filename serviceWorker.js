eee96a77a8d1d78716868fe8d4d7523d9fbee626
Gaseema Ndung'u <gaseema.n@gmail.com>
2017-02-23 12:12:41 +0300

    caching files

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,5 +1,24 @@
+var cacheName = 'v1';
+var cacheFiles = [
+    '/',
+    '/index.html',
+    '/css/app.css',
+    '/js/app.js'
+]
+
+
 self.addEventListener('install', function (e) {
     console.log("[ServiceWorker] Installed")
+
+    e.waitUntil(
+
+        caches.open(cacheName).then(function (cache) {
+
+            console.log("[ServiceWorker] Caching cacheFiles");
+            return cache.addAll(cacheFiles);
+
+        })
+    )
 })
 self.addEventListener('activate', function (e) {
     console.log("[ServiceWorker] Activated")
