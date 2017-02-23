1283bf8521f1a9f0bc6d1c7807bd37fc5a1620df
Gaseema Ndung'u <gaseema.n@gmail.com>
2017-02-23 11:56:51 +0300

    first commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 $(document).foundation()
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('/serviceWorker.js', {
+            scope: '/'
+        })
+        .then(function (registration) {
+            console.log("Service Worker Registered");
+        })
+        .catch(function (err) {
+            console.log("Service Worker Failed To Register", err);
+        })
+}
